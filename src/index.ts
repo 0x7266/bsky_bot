07089bd5b7714a19d6634d67221b2dbbffd36a84
rafael <rrrr0ffff@gmail.com>
2023-05-06 02:59:24 -0300

    it now handles multiple images

--- src/index.ts
+++ src/index.ts
@@ -17,38 +17,38 @@ const r = new Snoowrap({
 
 const stream = new SubmissionStream(r, {
 	subreddit: "mechanicalkeyboards",
-	limit: 5,
+	limit: 1,
 });
 
 stream.on("item", async (post: Snoowrap.Submission) => {
-	// console.log(post);
 	try {
 		// const { permalink, url, title, author, is_video, post_hint } = post;
 		// const url = "https://i.redd.it/ny2gw16elgxa1.png"; // test
-		if (post.is_video) {
-			console.log("🚫 New post is a video");
+		if (post.title === "Royal Kludge M75, pretty good budget board that requires very little modding." || post.title === "Just thought you all might get a kick out of this, that or have an aneurysm" || post.title === "Lolita Bow - Black On White Version") {
+			console.log("fuck this post");
 			return;
 		}
 		if ("is_gallery" in post && post.is_gallery) {
-			console.log(post);
+			const imgPaths: string[] = [];
 			const galleryPost = post as GallerySubmission;
 			const metadataKeys = galleryPost.media_metadata;
-			Object.keys(galleryPost.media_metadata).map(async (key) => {
+			for (const key of Object.keys(galleryPost.media_metadata)) {
 				const imageIdAndFormat = `${key}.${galleryPost.media_metadata[
 					key
 				].m.slice(6)}`;
+				imgPaths.push(`./images/${imageIdAndFormat}`);
 				await saveImage(imageIdAndFormat);
-				await postToBsky({
-					postUrl: `https://reddit.com${post.permalink}`,
-					title: post.title,
-					author: post.author.name,
-					authorUrl: `https://reddit.com/u/${post.author.name}`,
-					imgPath: `./images/${imageIdAndFormat}`,
-				});
+			}
+			await postToBsky({
+				postUrl: `https://reddit.com${post.permalink}`,
+				title: post.title,
+				author: post.author.name,
+				authorUrl: `https://reddit.com/u/${post.author.name}`,
+				imgPaths,
 			});
+			console.log({ imgPaths });
 		}
 		if (post.post_hint === "image") {
-			return;
 			const imgIdAndFormat = post.url.slice(18);
 			const imgPath = `./images/${imgIdAndFormat}`;
 			await saveImage(imgIdAndFormat);
@@ -57,8 +57,12 @@ stream.on("item", async (post: Snoowrap.Submission) => {
 				title: post.title,
 				author: post.author.name,
 				authorUrl: `https://reddit.com/u/${post.author.name}`,
-				imgPath: `./images/${imgIdAndFormat}`,
+				imgPaths: [`./images/${imgIdAndFormat}`],
 			});
+		}
+		if (post.is_video) {
+			console.log("🚫 New post is a video");
+			return;
 		}
 	} catch (error) {
 		console.error(error);
